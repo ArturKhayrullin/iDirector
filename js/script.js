719cab5669dc0f593a00e2129a746e075c369e8b
Khairullin Artur <Khairullin Artur>
2017-07-19 23:45:27 +0300

    -fixes -mobile version

--- js/script.js
+++ js/script.js
@@ -3,4 +3,32 @@ $(document).ready(function() {
       collapsible: true,
       active: false
     });
+
+    if ($(window).width()<=680) {
+    	$(".tabs .processes").click(function() {
+    		$(".in-processes").show();
+    		$(".in-events").hide();
+
+    		$(".tabs span").removeClass("active");
+    		$(this).addClass("active");
+    	});
+
+    	$(".tabs .events").click(function() {
+    		$(".in-events").show();
+    		$(".in-processes").hide();
+
+    		$(".tabs span").removeClass("active");
+    		$(this).addClass("active");
+    	});
+
+    	$(".tabs .processes").click();
+    }
+
+    $(".menu-opener").click(function() {
+    	$(".main-container .menu").show();
+    });
+
+    $(".main-container .menu .mobile-controls .menu-close").click(function() {
+    	$(".main-container .menu").hide();
+    });
 });
